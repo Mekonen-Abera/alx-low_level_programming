ffb86894eea67de05dd0f88659d9234d0fc0778a
Mekonen-Abera <makoninabera@gmail.com>
2023-08-10 06:38:30 -0700

    C Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-    * main - main block
-    * Return: 0
+    * Description: using the main function   
+    * This prints the size of various types on the computer it is compiled and run on
+    * Return: 0     
     */
 int main(void)
 {
