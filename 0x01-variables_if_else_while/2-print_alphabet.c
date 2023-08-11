f9258f27cba41386ef4ecdf174e36c2a6b4b4533
Mekonen-Abera <makoninabera@gmail.com>
2023-08-11 14:17:20 -0700

    #include <stdio.h>
    /**
     * main - Prints the alphabet in lowercase.
     *
     * Return: Always 0.
     */
    int main(void)
    {
    char letter;
    
    for (letter = 'a'; letter <= 'z'; letter++)
    putchar(letter);
    
    putchar('\n');
    
    return (0);
    vim 2-print_alphabet.c
    ls
    clear

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <ctype.h>
+
 /**
-*main - Program to print alphabet letter followed by new line
-*
-*Return: return 0
-*/
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
-	{
-		letter = tolower(letter);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	}
+
 	putchar('\n');
+
 	return (0);
 }
