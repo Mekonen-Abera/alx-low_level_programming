99933b0a55f68494319d461e9d62904dd59ff107
Mekonen-Abera <makoninabera@gmail.com>
2023-08-15 15:18:02 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,22 @@
     *        0 if the number is zero 
     *       -1 if number is less that zero 
     */ 
-  int print_sign(int n) 
-   {
-      if(n > 0)
-       {  
-      _putchar ('+');
-         return (1); 
-       } 
-    else if(n == 0)
-       {
-    _putchar ('0') ;
-     return (0);
-      }
-   else
-    {
-   _putchar ('-') ;
-   return (-1);
-    } 
- }
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
