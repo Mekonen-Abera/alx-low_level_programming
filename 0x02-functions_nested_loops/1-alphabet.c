1f14aea8ffdf31d27334ecb8207c69f511a3edaa
Mekonen-Abera <makoninabera@gmail.com>
2023-08-15 12:56:44 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  * Description: prints  in lowercase, followed by a new line
  * Return: 0 always
  */
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	int i; 
 
-	for (i = 97; i <= 122; i++)
+	for(i = 97; i <= 122; i++)
 	{
-		_putchar (i); 
+		_putchar(i); 
 	}
-	_putchar ('\n'); 
+	_putchar('\n'); 
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,4 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+#endif
