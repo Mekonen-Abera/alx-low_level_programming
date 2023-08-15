768c6f2fcca6a4c7e75a94a2bd4d4ff830e979ab
Mekonen-Abera <makoninabera@gmail.com>
2023-08-15 15:29:17 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i=1; i <=10;i++)
+	for(i= 1; i <= 10;i++)
 	{
-		for (j=97;j<=122;j++)
-			_putchar(j);
+	for(j= 97;j<= 122;j++)
+		_putchar(j);
 		_putchar('\n');
 	}
 }
