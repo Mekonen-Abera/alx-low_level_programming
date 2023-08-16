a52d9c30846c6f635b8a813224cce6a665b18af7
Mekonen-Abera <makoninabera@gmail.com>
2023-08-16 07:11:17 -0700

    C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main-create function that test the number is positive or negative
+ * Return:0
+ */
+int main (void)
+{
+	int i;
+	i=0;
+	positive_or_negative(i);
+	return (0);
+}
