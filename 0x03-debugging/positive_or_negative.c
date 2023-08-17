1388f8a890bcaab5b8154c619cb44c0d180a0702
Mekonen-Abera <makoninabera@gmail.com>
2023-08-17 01:44:02 -0700

    C-debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - random numbers
+* Return: 1 if the number is positive. 0 otherwise
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
