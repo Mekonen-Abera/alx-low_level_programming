37a237489fa449b52872f3e27f499693a062fd47
Mekonen-Abera <makoninabera@gmail.com>
2023-08-21 06:20:10 -0700

    C 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * puts2 - prints every character of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Noting
+ * Done by @Mekonen-Abera
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
