e498115551fc7ac8e2caa96b467c8c7b59be0c33
Mekonen-Abera <makoninabera@gmail.com>
2023-09-04 06:44:40 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,38 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+
 /**
- * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- * Return: 0
- * Done by @Mekonen-Abera
- */
+ * argstostr - a function that concatenates
+ *             all the arguments
+ *
+ * @ac: argument counter
+ * @av: argument holder
+ *
+ * Return: a pointer to a new string
+ *         or NULL if it fails
+*/
+
 char *argstostr(int ac, char **av)
 {
-    int i, n, r = 0, l = 0;
-    char *str;
-    if (ac == 0 || av == NULL)
-        return (NULL);
-    for (i = 0; i < ac; i++)
-    {
-        for (n = 0; av[i][n]; n++)
-            l++;
-    }
-    l += ac;
-    str = malloc(sizeof(char) * l + 1);
-    if (str == NULL)
-        return (NULL);
-    for (i = 0; i < ac; i++)
-    {
-        for (n = 0; av[i][n]; n++)
-        {
-            str[r] = av[i][n];
-            r++;
-        }
-        if (str[r] == '\0')
-        {
-            str[r++] = '\n';
-        }
-    }
-    return (str);
+	int i, j, k, length;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
 }
