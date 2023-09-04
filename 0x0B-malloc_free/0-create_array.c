bb3d680acdf27f671a77d7233c69b17c6b4bceea
Mekonen-Abera <makoninabera@gmail.com>
2023-09-04 04:43:56 -0700

    C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,21 @@
 #include "main.h"
-#include <stdlib.h>	
-/**	
- * create_array - create array of size size and assign char c	
- * @size: size of array	
- * @c: char to assign	
- * Description: creat array of size size and assign char c	
- * Return: pointer to array, NULL if fail	
- *	
+#include <stdlib.h>
+/**
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: creat array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ * Done by @Mekonen-Abera
  */
-		
-char *create_array(unsigned int size, char c)	
+char *create_array(unsigned int size, char c)
 {
-		
-	char *str;
-	unsigned int i;
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);	
+    char *str;
+    unsigned int i;
+    str = malloc(sizeof(char) * size);
+    if (size == 0 || str == NULL)
+        return (NULL);
+    for (i = 0; i < size; i++)
+        str[i] = c;
+    return (str);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,22 +1,20 @@
-#include <stdio.h>	
-#include <stdlib.h>	
-#include "main.h"	
-/**	
- * free_grid - frees 2d array	
- * @grid: 2d grid	
- * @height: height dimension of grid	
- * Description: frees memory of grid	
- * Return: nothing	
- *	By Mekonen-Abera
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *	Done by @Mekonen-Abera
  */
-void free_grid(int **grid, int height)		
-{	
-	int i;	
-	for (i = 0; i < height; i++)	
-	{
-		
-		free(grid[i]);
-	}
-		
-	free(grid);	
+void free_grid(int **grid, int height)
+{
+    int i;
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+    free(grid);
 }
