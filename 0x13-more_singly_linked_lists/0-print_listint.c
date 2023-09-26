2188445624a2c62e2646b3e1de2790fe0922ae66
Mekonen-Abera <makoninabera@gmail.com>
2023-09-26 15:53:35 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,19 @@
 /**
  * print_listint - A function that prints all the elements
  * @h: linked list of type listint_t to be printed
+ *
  * Done by: Mekonen-Abera
- * Return: number of nodes
+ * Return:the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
-	}
-	return (num);
+        size_t num = 0;
+        while (h)
+        {
+                printf("%d\n", h->n);
+                num++;
+                h = h->next;
+        }
+        return (num);
 }
