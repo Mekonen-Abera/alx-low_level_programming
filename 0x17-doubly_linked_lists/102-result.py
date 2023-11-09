dad9f003eba3594a489e1bd9e625306da16a382b
Mekonen-Abera <makoninabera@gmail.com>
2023-11-09 04:22:07 -0800

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,10 +1,11 @@
 #!/user/bin/python3
 largest_palindrome = 0
-for i in range(100,1000):
-    for j in range(100,1000):
-        produnt = i*j
-        if str(product) == str(product)[::-1] and product > largest_palindrome:
-            
-            #save the result in the file
-            with open("102-result","w")as file:
-                file.write(str(largest_palindrome))
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i*j
+        if str(product) == str(product)[:: -1] and product > largest_palindrome:
+            largest_palindrome = product
+
+            # save the result in the file
+    with open("102-result", "w") as file:
+        file.write(str(largest_palindrome))
