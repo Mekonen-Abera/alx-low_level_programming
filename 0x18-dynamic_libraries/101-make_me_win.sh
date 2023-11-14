8588eac2c381a9c8fd0cfa8112bf01feee54fc9a
Mekonen-Abera <makoninabera@gmail.com>
2023-11-13 16:46:01 -0800

    Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Mekonen-Abera/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P .. https://raw.githubusercontent.com/Mekonen-Abera/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
